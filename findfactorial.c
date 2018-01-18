ae8e1c659e17e22959ce20ad9dc841e8482845ff
SanaaAliAbdo <35563900+SanaaAliAbdo@users.noreply.github.com>
2018-01-18 13:37:10 +0200

    Update findfactorial.c

--- findfactorial.c
+++ findfactorial.c
@@ -1,30 +1,31 @@
 # include <stdio.h>
+
 //declare a function that find the factorial of the number
 int factorial (int num);
 
 //============================
 int main()
 {
-    unsigned int Num1, tmp;
+    	unsigned int Num1, tmp;
 
-    printf("ENTER the Nuber its factorial you want : ");
-    scanf("%d",&Num1);
-    tmp=Num1;
-    printf(" **************************************** \n ");
-    printf(" The factorial of %d is %d \n",tmp,factorial(Num1));
+    	printf("ENTER the Nuber its factorial you want : ");
+    	scanf("%d",&Num1);
+    	tmp=Num1;
+    	printf(" **************************************** \n ");
+    	printf(" The factorial of %d is %d \n",tmp,factorial(Num1));
    
   
-    return 0;
+	return 0;
     
 }
 //==========================
 int factorial(int num){
 	 unsigned int  retfactorial=1;
-	 while (num>0)
-    {
-        retfactorial *= num;
-        num = num - 1;
-    }
-    return retfactorial;
+	while (num>0)
+    	{
+        	retfactorial *= num;
+        	num = num - 1;
+    	}
+    	return retfactorial;
   
 }
